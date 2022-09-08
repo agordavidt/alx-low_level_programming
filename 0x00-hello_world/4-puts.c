cee5ae16432502cab2c4c8f5d122c07f2ebd97c0
agordavidt <agordavid14@gmail.com>
2022-09-08 15:25:58 -0700

    He, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,27 +2,41 @@
 
 
 
+
+
+
+
 /**
 
-* main - print the string in the put function
+ * main - Entry point
 
-*
+ *
 
-* Description: using the main function
+ * Return: Always 0 (Success)
 
-* this program prints "Programming is like building a multilingual puzzle
+ *
 
-* Return: 0
+ */
 
-*/
+
+
+
+
+
 
 int main(void)
 
+
+
 {
 
-	printf("with proper grammar, but the outcome is a piece of art,\n");
 
-	return (0);
 
-}
+		puts("\"Programming is like building a multilingual puzzle");
+
+
+
+			return (0);
+
+
 
