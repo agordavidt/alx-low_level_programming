da217e7af3385e3f5ef2f9dd88eb64901086b863
agordavidt <98212267+agordavidt@users.noreply.github.com>
2022-09-10 05:36:42 +0100

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,14 +5,14 @@
 * Numbers must be separated by commas and a space.
 * The 3 digits myst be diffeent.
 * 012, 120, 102, 021, 210 are considered the same combination.
-* print only the smallest combinatin of 3 digits.
+* you are not allowed to use any variable of type char.
 * Numbers should be printed in ascending order.
 * You can only use 'putchar' to print to console.
 * Yu can only use 'putcahr' up to 6 times.
 8 You are not allowed to use any variables of type 'char'.
 * Return: 0
 */
-int main (void)
+int main(void)
 {
 	int i, j, k, l;
 	
