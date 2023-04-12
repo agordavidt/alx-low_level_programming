628f59d786c3e41639f65449a5aeabe7f94f3dff
agordavidt <agordavid14@gmail.com>
2023-04-11 23:29:16 -0700

    task 10

---
+++ 0x1E-search_algorithms/103-exponential.c
@@ -0,0 +1,13 @@
+#include "search_algos.h"
+/**
+ * b_search - function that searches for a value in a sorted
+ * array of integers using the Binary search algorithm
+ *
+ * @array: Array is a pointer to the first element of the array to search in
+ * @flow: Left index in the array
+ * @high: right index in the array
+ *
+ * Return: The first index where value is located
+ * or -1 if value is not found or if array is NULL
+ */
+
